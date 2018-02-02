415757c64a2a939cb3792a108052c80a32656c90
sijju13 <sijjukarki@gmail.com>
2018-02-02 00:22:24 -0500

    hw 1, #3

--- homework1/GreatCircle.java
+++ homework1/GreatCircle.java
@@ -7,6 +7,16 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class GreatCircle {
     public static void main(String[] args) {
-        ...
+        double a = Double.parseDouble(args[0]);
+        double b = Double.parseDouble(args[1]);
+        double c = Double.parseDouble(args[2]);
+        double d = Double.parseDouble(args[3]);
+        double x1 = Math.toRadians(a);
+        double y1 = Math.toRadians(b);
+        double x2 = Math.toRadians(c);
+        double y2 = Math.toRadians(d);
+        double D = 111* Math.acos(Math.sin(x1) * Math.sin(x2) + Math.cos(x1) * Math.cos(x2) * Math.cos(y1-y2));
+        double C = Math.toDegrees(D);
+        StdOut.println(C);
     }
-}
+} 
